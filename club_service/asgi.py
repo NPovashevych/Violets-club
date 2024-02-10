a04e7a4f302d5047e28c3f95b6ede7a769829974
Nataliia Povashevych <interchanal@ukr.net>
2024-02-11 00:42:35 +0200

    add first_draft project

--- club_service/asgi.py
+++ club_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for club_servise project.
+ASGI config for club_service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_servise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_service.settings')
 
 application = get_asgi_application()
--- club_service/wsgi.py
+++ club_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for club_servise project.
+WSGI config for club_service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_servise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_service.settings')
 
 application = get_wsgi_application()
