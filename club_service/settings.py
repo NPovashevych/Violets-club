7d40e082bb64fe60bdae4ab0f81fbc7dcd9bf476
Nataliia Povashevych <interchanal@ukr.net>
2024-02-28 17:20:12 +0200

    change debug in settings and change requirements.txt

--- club_service/settings.py
+++ club_service/settings.py
@@ -28,16 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1",]
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+INTERNAL_IPS = ["127.0.0.1",]
 
 # Application definition
 
