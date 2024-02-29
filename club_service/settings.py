034e13847f74785e79f64c8ed81fcd4ab0d7212c
Nataliia Povashevych <interchanal@ukr.net>
2024-02-29 16:34:01 +0200

    change allowed hosts

--- club_service/settings.py
+++ club_service/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1",]
+ALLOWED_HOSTS = ["127.0.0.1", "https://violets-club-2.onrender.com",]
 
 INTERNAL_IPS = ["127.0.0.1",]
 
