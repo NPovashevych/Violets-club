5c6a44e1deb999d8877c374c6e4610d4a1340afb
Nataliia Povashevych <interchanal@ukr.net>
2024-02-29 19:28:45 +0200

    debug false again

--- club_service/settings.py
+++ club_service/settings.py
@@ -32,7 +32,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "violets-club-2.onrender.com",]
 
